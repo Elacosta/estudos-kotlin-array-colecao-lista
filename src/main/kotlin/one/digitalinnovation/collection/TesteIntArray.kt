abacdb28c1765f18729e811d4e14d8f93238d7d7
Elacosta <neero5500@gmail.com>
2021-09-30 23:39:52 -0300

    first commit

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteIntArray.kt
@@ -0,0 +1,32 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 2
+    values[2] = 3
+    values[3] = 4
+    values[4] = 5
+
+    for (valor in values) {
+        println(valor)
+    }
+    println("------------")
+    values.forEach { //recurso de lambda do Kotlin
+        println(it)
+    }
+    println("------------")
+    values.forEach { valor -> //valor se torna It
+        println(valor)
+    }
+    println("------------")
+    for (index in values.indices) { //iterar sobre os índices do array
+        println(values[index])
+    }
+    println("------------")
+    values.sort() //recurso de um array que faz o array ir do menos para o maior
+    for (valor in values) {
+        println(valor)
+    }
+}
